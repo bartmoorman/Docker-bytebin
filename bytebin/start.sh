06c16aa155679cd6c6309ed29e3585cf091cd4aa
Bart Moorman <github@id10t.us>
2020-10-07 12:25:55 -0600

    Become PID 1

--- bytebin/start.sh
+++ bytebin/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-jq \
+$(which jq) \
 --arg host ${BYTEBIN_HOST:-0.0.0.0} \
 --argjson port ${BYTEBIN_PORT:-8080} \
 --argjson keyLength ${BYTEBIN_KEY_LENGTH:-7} \
@@ -7,4 +7,4 @@ jq \
 --argjson maxContentLengthMb ${BYTEBIN_MAX_CONTENT_LENGTH_MB:-10} \
 '.host = $host | .port = $port | .keyLength = $keyLength | .lifetimeMinutes = $lifetimeMinutes | .maxContentLengthMb = $maxContentLengthMb' <<< '{}' > config.json
 
-$(which java) -jar /opt/bytebin/bytebin.jar
+exec $(which java) -jar /opt/bytebin/bytebin.jar
